743bc65b86f825ff4c8fdcc16456e9319599d26f
belulok <belulok_sebastian@hotmail.com>
2025-05-06 18:55:07 +0800

    Revert "props - latest"
    This reverts commit d1f33e04d3ef6a65e36cf23a4fa7257b91640487.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header.jsx';
-import CoreConcept from './components/CoreConcept';
-import TabButton from './components/TabButton';
-import { useState } from 'react';
-import { EXAMPLES } from './data.js';
-
 function App() {
   const [ selectedTopic, setSelectedTopic ] = useState('components');
 
@@ -17,37 +10,16 @@ function App() {
 
   return (
     <div>
-      <Header />
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          Fundamental React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept 
-            title={CORE_CONCEPTS[0].title}
-            description={CORE_CONCEPTS[0].description}
-            image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleClick('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleClick('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleClick('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleClick('state')}>State</TabButton>
-          </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
-        </section>
+        <h2>Time to get started!</h2>
       </main>
     </div>
   );
