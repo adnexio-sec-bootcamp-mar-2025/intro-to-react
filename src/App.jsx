79391233b4e1225e4db0aa40d954e1412cc488c3
belulok <belulok_sebastian@hotmail.com>
2025-05-08 21:32:43 +0800

    completed section 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import Header from './components/Header';
-import { CORE_CONCEPTS } from './data';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
+import { useState } from 'react';
+
 
  function App() {
+  const [title, setTitle] = useState('components');
+  // const name = 'Components';
+
+  function handleClick(name) {
+    // name = 'JSX';
+    setTitle(name);
+    console.log(name);
+  }
 
   return (
     <div>
@@ -15,6 +26,31 @@ import CoreConcept from './components/CoreConcept';
               <CoreConcept key={concepItem.title} {...concepItem}/>
             ))}
           </ul>
+        </section>
+        <section id="examples">
+
+          <h2>Examples</h2>
+
+          <menu>
+            <TabButton onSelect={function(){handleClick('components')}}>Components</TabButton>
+            <TabButton onSelect={function(){handleClick('jsx')}}>JSX</TabButton>
+            <TabButton onSelect={function(){handleClick('props')}}>Props</TabButton>
+            <TabButton onSelect={function(){handleClick('state')}}>State</TabButton>
+          </menu>
+
+          {/* event listeners & event handlers */}
+          {/* {document.querySelector('button').addEventListener('click', function() {
+            console.log('button clicked');
+          });} */}
+
+          <div id="tab-content">
+            <h3>{EXAMPLES[title].title}</h3>
+            <p>{EXAMPLES[title].description}</p>
+            <pre>
+              <code>{EXAMPLES[title].code}</code>
+            </pre>
+          </div>
+
         </section>
       </main>
     </div>
